4fe30dec4eb7ae3d19e1b6dd29449595124e44ed
Alwaz <alwazkazi3@gmail.com>
2021-11-09 08:21:59 -0800

    bugs fixed

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Location from "./Location";
 import "../index.css";
 import axios from "axios";
@@ -6,44 +6,56 @@ import axios from "axios";
 const Form = () => {
   const [inputValue, setInputValue] = useState("");
   const [weatherData, setWeatherData] = useState(null);
+  const location = useRef();
+
+  // https://api.openweathermap.org/data/2.5/weather?q=&units=metric&appid=108dd9a67c96f23039937fe6f3c91963
 
   const API_KEY = `108dd9a67c96f23039937fe6f3c91963`;
   const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?`;
 
   const URL = `${BASE_URL}q=${inputValue}&units=metric&appid=${API_KEY}`;
 
-  const getValue = (e) => {
-    setInputValue(e.target.value);
-  };
-
   // function to get data
   const getData = async () => {
-    const { data } = await axios.get(URL);
-    setWeatherData(data);
+    if (inputValue != "") {
+      const { data } = await axios.get(URL);
+      setWeatherData(data);
+    }
   };
 
+  // data will be fetched on page load
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+
   useEffect(() => {
+    console.log(inputValue);
     getData();
-  }, []);
+  }, [inputValue]);
 
   return (
     <>
       <div className="search_box">
         <input
-          value={inputValue}
           type="text"
           className="search_bar"
           placeholder="Enter Location..."
-          onChange={getValue}
+          ref={location}
         />
-        <button className="search_btn" type="submit" onClick={() => getData()}>
+        <button
+          className="search_btn"
+          type="submit"
+          onClick={() => {
+            setInputValue(location.current.value);
+          }}
+        >
           <i class="fas fa-search-location"></i>
         </button>
       </div>
 
       {!inputValue ? (
-        ""
-      ) : (
+        <h3>Enter city name to know Weather.</h3>
+      ) : weatherData ? (
         <Location
           city={weatherData.name}
           country={weatherData.sys.country}
@@ -51,6 +63,8 @@ const Form = () => {
           icon={weatherData.weather[0].icon}
           weather={weatherData.weather[0].description}
         />
+      ) : (
+        <></>
       )}
     </>
   );
